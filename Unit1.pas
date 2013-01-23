f3359f1cbc837384f8525b458f6b32279acf5b41
Ján Jakabovic <janyjakabovic@gmail.com>
2013-01-23 20:01:47 +0100

    upload

---
+++ Unit1.pas
@@ -0,0 +1,71 @@
+unit Unit1;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Edit1: TEdit;
+    Button1: TButton;
+    Label1: TLabel;
+    Button2: TButton;
+    Label2: TLabel;
+    Edit2: TEdit;
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+var zvysok,desiatkova:integer;
+    vysledok:string;
+begin
+try
+vysledok:='';
+desiatkova:=strtoint(edit1.text);
+repeat
+begin
+zvysok:=desiatkova mod 2;
+vysledok:=inttostr(zvysok)+vysledok;
+desiatkova:=desiatkova div 2;
+end;
+until desiatkova=0;
+label1.Caption:=vysledok;
+ except showmessage('Tam m· byù ËÌslo!');
+end;
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+var i,d:integer;
+    dvojkova:string;
+    mocnina,sucet:real;
+begin
+sucet:=0;
+mocnina:=0.5;
+   dvojkova:=edit2.Text;
+   d:=length(dvojkova);
+   for i:= d downto 1 do
+   begin
+         mocnina:=mocnina*2;
+         if dvojkova[i]='1' then begin
+         sucet:=mocnina+sucet;
+         end;
+         label2.caption:=floattostr(sucet);
+      end;
+
+end;
+
+end.
---
+++ Unit1.~dfm
@@ -0,0 +1,62 @@
+object Form1: TForm1
+  Left = 192
+  Top = 114
+  Width = 696
+  Height = 480
+  Caption = 'Form1'
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'MS Sans Serif'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Label1: TLabel
+    Left = 160
+    Top = 272
+    Width = 32
+    Height = 13
+    Caption = 'Label1'
+  end
+  object Label2: TLabel
+    Left = 432
+    Top = 272
+    Width = 32
+    Height = 13
+    Caption = 'Label2'
+  end
+  object Edit1: TEdit
+    Left = 112
+    Top = 136
+    Width = 121
+    Height = 21
+    TabOrder = 0
+  end
+  object Button1: TButton
+    Left = 80
+    Top = 192
+    Width = 193
+    Height = 57
+    Caption = 'Z Desiatkovej do Dvojkovej'
+    TabOrder = 1
+    OnClick = Button1Click
+  end
+  object Button2: TButton
+    Left = 360
+    Top = 192
+    Width = 177
+    Height = 57
+    Caption = 'Z Dvojkovej do Desiatkovej'
+    TabOrder = 2
+    OnClick = Button2Click
+  end
+  object Edit2: TEdit
+    Left = 392
+    Top = 136
+    Width = 121
+    Height = 21
+    TabOrder = 3
+  end
+end
